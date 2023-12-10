cc750647477f83e3b4f43b857dd65304bdf20394
Çağatay <durucagatay01@gmail.com>
2023-12-10 16:06:49 +0300

    Project structure done, Files initiated

---
+++ app/src/main/java/com/ozyegin/project/MainFragment.kt
@@ -0,0 +1,28 @@
+package com.ozyegin.project
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Navigate to the Search Fragment
+        val navController = Navigation.findNavController(view)
+        navController.navigate(R.id.action_mainFragment_to_searchFragment)
+    }
+}
---
+++ app/src/main/java/com/ozyegin/project/SearchFragment.kt
@@ -0,0 +1,6 @@
+package com.ozyegin.project
+
+import androidx.fragment.app.Fragment
+
+class SearchFragment: Fragment() {
+}
