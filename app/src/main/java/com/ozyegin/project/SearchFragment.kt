44524bc6c30f91d2239a1844a7a26c4d229b7059
Çağatay <durucagatay01@gmail.com>
2023-12-10 16:27:43 +0300

    Search Fragment done

--- app/src/main/java/com/ozyegin/project/SearchFragment.kt
+++ app/src/main/java/com/ozyegin/project/SearchFragment.kt
@@ -1,6 +1,36 @@
-package com.ozyegin.project
-
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.ozyegin.project.R
+import com.ozyegin.project.viewmodels.SearchViewModel
 
-class SearchFragment: Fragment() {
+class SearchFragment : Fragment() {
+
+    private lateinit var viewModel: SearchViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.search_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this)[SearchViewModel::class.java]
+
+        val searchView = view.findViewById<SearchView>(R.id.search_view)
+        val gameList = view.findViewById<RecyclerView>(R.id.game_list)
+
+        // For the future with api integration to view model function search etc will implement
+
+    }
 }
---
+++ app/src/main/java/com/ozyegin/project/viewmodels/SearchViewModel.kt
@@ -0,0 +1,10 @@
+package com.ozyegin.project.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel : ViewModel() {
+
+
+}
